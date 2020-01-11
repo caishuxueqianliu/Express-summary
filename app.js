1fe1ecf94317bf9facca9c72922be8e6568bf73a
caishuxueqianliu <470290171@qq.com>
2020-01-11 17:16:50 +0800

    1

--- app.js
+++ app.js
@@ -3,42 +3,52 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var session = require('express-session')
+var bodyParser = require('body-parser')
 var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
-//var user=require('./mysql/mysql.js')
+
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
+var svgCaptcha = require('svg-captcha');
 
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
-//app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+
+app.use(session({
+  secret: 'itcast',
+  resave: false,
+  saveUninitialized: false // 无论你是否使用 Session ，我都默认直接给你分配一把钥匙
+}))
+
+
+app.get('/captcha', function (req, res) {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+    var captcha = svgCaptcha.create({
+    	size:4, //验证码长度
+        fontSize:50,
+        width:120,
+        height:34,
+        background:"#cc9966"
+    });
+
+    req.session.captcha = captcha.text;    
+    res.type('svg');
+  res.status(200).send(captcha.data);
+  console.log(req.session.captcha)
+
+
 });
 
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.post('/captcha', function (req, res) {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+console.log(req.body.yzm)
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-
+  
+});
 app.listen(3000,()=>{
 console.log('success...')})
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,35 @@ var user = require('../mysql/mysql.js');
 
 
 
-// router.get('/addUser', function(req, res, next) {
-//     user.add(req, res, next);
-// });
+
+// router.get("/code",async (ctx)=>{
+//     let captcha = svgCaptcha.create({
+//         size:4, //验证码长度
+//         fontSize:50,
+//         width:120,
+//         height:34,
+//         background:"#cc9966"
+//     });
+//     /*加法验证码 */
+//     // let captcha = svgCaptcha.createMathExpr({
+//     //     size:4, //验证码长度
+//     //     fontSize:50,
+//     //     width:100,
+//     //     height:40,
+//     //     background:"#cc9966"
+//     // });
+//     console.log(captcha.text)
+//     ctx.session.code = captcha.text;
+//     ctx.response.type="image/svg+xml";
+//     ctx.body = captcha.data;
+
+// })
+
+ 
+
+
 router.get('/', function(req, res, next) {
+	res.setHeader("Access-Control-Allow-Origin", "*");
     user.queryAll(req, res, next);
 });
 
